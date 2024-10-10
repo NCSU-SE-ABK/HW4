ffbfaeb066a25ccec304dc2145824fe1782411e2
karthikeya7746 <104351752+karthikeya7746@users.noreply.github.com>
2024-10-10 12:33:49 -0400

    Create task3.sh

---
+++ task3.sh
@@ -0,0 +1,1 @@
+awk -F',' 'NR > 1 && $3==2 && substr($13, 1, 1) == "S" {print $0}' titanic.csv | sed 's/male/M/g; s/female/F/g' | awk -F',' '{ print $0 } $7 != "" { total += $7; count++ } END {print "Average Age:", total/count}'
